3bb2c0ed6a6ff8a8ea95451b2c8898c12c1a98f0
HoangLC3 <hoanglc3@fpt.com.vn>
2023-07-18 11:01:56 +0700

    edit change link websocket

--- src/context/WSContext.ts
+++ src/context/WSContext.ts
@@ -4,6 +4,6 @@ import { io, Socket } from "socket.io-client";
 const { REACT_APP_WEBSOCKET_URL } = process.env;
 const WS_URI = REACT_APP_WEBSOCKET_URL || "";
 
-export const socket = io(WS_URI);
+export const socket = io(WS_URI, { transports: ["websocket"] });
 export const WebSocketContext = createContext<Socket>(socket);
 export const WebSocketProvider = WebSocketContext.Provider;
